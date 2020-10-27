b03419146ccd6b9da7b243f0a7b96101e901ea0f
supriyanig <reach.supriya.nigam@gmail.com>
2020-10-27 15:20:40 +0530

    Delete AppFooter.spec.js

--- tests/e2e/specs/AppFooter.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Test App Footer Component', () => {
-  it('Check If All the components are Displayed or not', () => {
-    cy.visit('http://localhost:8080').then(() => {
-      cy.get('div > footer  strong').then((res) => {
-        expect(res[0].innerText).to.contain('All Tv Shows')
-      })
-    })
-  })
-})
