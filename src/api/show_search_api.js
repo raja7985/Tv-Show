52e1d58b300c505ae7cc6f5a94e9d9f7170f0eae
supriyanig <reach.supriya.nigam@gmail.com>
2020-10-27 12:15:59 +0530

    Delete show_search_api.js

--- src/api/show_search_api.js
+++
@@ -1,7 +0,0 @@
-import api from '.'
-const END_POINT = '/search/shows?q='
-// Get Shows Results
-const getTvShowsOnSearch = searchKeyword => api.get(END_POINT + searchKeyword)
-export {
-  getTvShowsOnSearch
-}
